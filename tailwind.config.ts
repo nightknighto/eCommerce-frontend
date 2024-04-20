4e615d32d5a1a54339c3237fb2abdda3d80ffd7d
NightKnight <Ahmedatwa866@yahoo.com>
2024-04-20 16:12:39 +0200

    Initial structure of the main-page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
+const flowbite = require("flowbite-react/tailwind");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   darkMode: "class",
   theme: {
@@ -330,6 +332,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 };
 export default config;
