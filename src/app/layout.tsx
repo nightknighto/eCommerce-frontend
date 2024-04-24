6456bbc85cff7830eaa20156514f604400662b15
Mahmoud Hamdy <90795679+MahmoudHamdy02@users.noreply.github.com>
2024-04-24 19:39:00 +0200

    Remove hardcoded loading page (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
-import Loader from "@/components/common/Loader";
+import React, { useState } from "react";
 
 export default function RootLayout({
   children,
@@ -12,19 +11,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [loading, setLoading] = useState<boolean>(true);
 
   // const pathname = usePathname();
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? <Loader /> : children}
+          {children}
         </div>
       </body>
     </html>
