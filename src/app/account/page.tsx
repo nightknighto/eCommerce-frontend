7fcedb8ac3ce91d731258480d0e4224f25823b20
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-05-01 20:49:56 +0300

    add header, profile button and more products

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,6 @@
 import ProductCard from "@/components/Cards/ProductCard";
 import MainLayout from "@/components/Layouts/MainLayout";
+import { Button } from "flowbite-react";
 
 const StatsCard = (props: {topText: string, bottomText: string}) => {
     return (
@@ -13,7 +14,16 @@ const StatsCard = (props: {topText: string, bottomText: string}) => {
 const Account = () => {
     return (
         <MainLayout>
-            <div className="flex justify-center p-8">
+            <div className="flex flex-col items-center p-8">
+                <div className="flex w-full justify-between mb-4">
+                    <p className="text-2xl font-bold">My Account</p>
+                    <Button
+                        color="blue"
+                        href="/profile"
+                    >
+                        View Profile
+                    </Button>
+                </div>
                 <div className="w-full border-slate-200 border-2 flex flex-col rounded-md p-4">
                     <p className="text-xl font-semibold mb-4">Statistics</p>
                     <div className="flex gap-x-4 mb-8">
@@ -22,10 +32,17 @@ const Account = () => {
                         <StatsCard topText="Total revenue" bottomText="$3,472"/>
                         <StatsCard topText="Remaining stock" bottomText="846"/>
                     </div>
+                    
                     <p className="text-xl font-semibold mb-4">Listed Products</p>
-                    <div className="flex justify-around gap-x-2">
+                    <div className="flex justify-around gap-x-2 mb-8">
                         <ProductCard />
                         <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                    </div>
+                    
+                    <p className="text-xl font-semibold mb-4">Products low on stock</p>
+                    <div className="flex gap-x-2">
                         <ProductCard />
                         <ProductCard />
                     </div>
