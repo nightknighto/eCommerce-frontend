f7c10a8b497a571ea263e7cff38e387ef06ebc7c
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-05-06 18:53:02 +0300

    activate add to cart button

--- src/app/productPage/[id]/page.tsx
+++ src/app/productPage/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, MouseEvent, useEffect} from "react";
+import React, { useState, MouseEvent, useEffect, useContext} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Review from "./Review";
@@ -8,9 +8,15 @@ import ReviewWindow from "./ReviewWindow";
 import RelatedItem from "./RelatedItem";
 import { Product } from "@/types/product";
 import { useParams } from "next/navigation";
+import { CartButtonContext } from "@/contexts/CartButtonContext";
 
 const ProductPage = () =>{
     const searchParams = useParams<{id: string}>();
+
+    const {addToCart, cartData} = useContext(CartButtonContext);
+    const productId = parseInt(searchParams.id)
+    const isInCart = cartData.cart.some(item => item.product_details.id === productId);
+
     const [product, setProduct] = useState<Product>();
     const [reviewDisplay,setReviewDisplay] = useState(false);
     const [rate,setRate] = useState(4)
@@ -19,13 +25,13 @@ const ProductPage = () =>{
     }
 
     useEffect(() => {
-        fetch(`https://distributed-project-backend.onrender.com/api/home/products/${parseInt(searchParams.id)}`)
+        fetch(`https://distributed-project-backend.onrender.com/api/home/products/${productId}`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
             setProduct(data);
         })
-    }, [searchParams.id])
+    }, [productId])
 
 
     return(
@@ -57,7 +63,16 @@ const ProductPage = () =>{
                                     <span>Rating:&nbsp;</span>
                                     <span>4.3</span>
                                 </div>
-                                <button className="bg-sky-500 hover:bg-white hover:text-inherit border-2 duration-500 border-sky-500 w-50 h-13 text-white text-3xl">Add To Cart</button>
+                                {isInCart ? 
+                                <span className="text-gray-900 dark:text-white text-2xl font-bold">Added to cart!</span>
+                                :
+                                <button 
+                                    className="bg-sky-500 hover:bg-white hover:text-inherit border-2 duration-500 border-sky-500 w-50 h-13 text-white text-3xl"
+                                    onClick={() => addToCart(productId)}
+                                >
+                                    Add To Cart
+                                </button>
+                                }
                                 <button className="flex flex-row justify-center items-center w-fit gap-x-2 w-fit my-1 h-10 text-xl hover:text-2xl duration-500">
                                     <span>Add To Wishlist</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="rgb(100 116 139)" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-501Zm0 381L313-234q-72-65-123.5-116t-85-96q-33.5-45-49-87T40-621q0-94 63-156.5T260-840q52 0 99 22t81 62q34-40 81-62t99-22q81 0 136 45.5T831-680h-85q-18-40-53-60t-73-20q-51 0-88 27.5T463-660h-46q-31-45-70.5-72.5T260-760q-57 0-98.5 39.5T120-621q0 33 14 67t50 78.5q36 44.5 98 104T440-228q26-23 61-53t56-50l9 9 19.5 19.5L605-283l9 9q-22 20-56 49.5T498-172l-58 52Zm280-160v-120H600v-80h120v-120h80v120h120v80H800v120h-80Z"/></svg>
